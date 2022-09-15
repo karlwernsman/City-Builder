bbcada13e7d46b2af252237f8f5a3ad3594432f1
karl wernsman <wernsmank@gmail.com>
2022-09-15 10:42:51 -0700

    Climate images changing with dropdown and background color change functioning

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 /* Imports */
 
 /* Get DOM Elements */
-//const article = document.getElementById('display');
+const article = document.getElementById('display');
 const cityName = document.getElementById('city-name');
 const nameInput = document.getElementById('name-input');
+const climateSelect = document.getElementById('climate-select');
+const climateImg = document.getElementById('climate-img');
+
 /* State */
 const city = {
     name: '',
@@ -17,9 +20,16 @@ nameInput.addEventListener('input', () => {
     displayCity();
 });
 
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
+    displayCity();
+});
 /* Display Functions */
 function displayCity() {
+    article.classList = '';
+    article.classList.add(city.climate);
     cityName.textContent = city.name;
+    climateImg.src = 'assets/' + city.climate + '-climate.jpeg';
 }
 // (don't forget to call any display functions you want to run on page load!)
 displayCity();
