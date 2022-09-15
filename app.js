b47f2430de8248bbefad2195c6bc59349a8f0450
karl wernsman <wernsmank@gmail.com>
2022-09-15 10:21:09 -0700

    Name display JS functioning

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+//const article = document.getElementById('display');
+const cityName = document.getElementById('city-name');
+const nameInput = document.getElementById('name-input');
 /* State */
-
+const city = {
+    name: '',
+    climate: '',
+    architecture: '',
+    attractions: '',
+};
 /* Events */
+nameInput.addEventListener('input', () => {
+    city.name = nameInput.value;
+    displayCity();
+});
 
 /* Display Functions */
-
+function displayCity() {
+    cityName.textContent = city.name;
+}
 // (don't forget to call any display functions you want to run on page load!)
+displayCity();
